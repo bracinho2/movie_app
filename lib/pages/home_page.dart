cf50ea66b055a2f3c8a3c95f878b2fc1d6fdd2da
bracinho2 <bracinho2@hotmail.com>
2022-06-13 07:46:39 -0300

    Criação da lista de movies com cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:movie_app/controllers/movie_controller.dart';
 import 'package:movie_app/models/movies_model.dart';
 import 'package:movie_app/repositories/movies_repository_impl.dart';
 import 'package:movie_app/service/dio_service_impl.dart';
+import 'package:movie_app/utils/apis.utils.dart';
+import 'package:movie_app/widgets/custom_list_card_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,19 +19,40 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ValueListenableBuilder<Movies?>(
-          valueListenable: _controller.movies,
-          builder: (_, movies, __) {
-            return movies != null
-                ? ListView.builder(
-                    itemCount: movies.listMovies.length,
-                    itemBuilder: ((context, index) =>
-                        Text(movies.listMovies[index].title)),
-                  )
-                : const Center(
-                    child: CircularProgressIndicator(),
-                  );
-          }),
+      body: Padding(
+        padding: const EdgeInsets.all(28),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 40),
+              Text(
+                'Movies',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+              ValueListenableBuilder<Movies?>(
+                  valueListenable: _controller.movies,
+                  builder: (_, movies, __) {
+                    return movies != null
+                        ? ListView.separated(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: movies.listMovies.length,
+                            itemBuilder: ((context, index) =>
+                                CustomListCardWidget(
+                                    movie: movies.listMovies[index])),
+                            separatorBuilder:
+                                (BuildContext context, int index) =>
+                                    const Divider(),
+                          )
+                        : const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                  }),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/custom_list_card_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:movie_app/models/movies_model.dart';
+import 'package:movie_app/utils/apis.utils.dart';
+
+class CustomListCardWidget extends StatelessWidget {
+  final Movie movie;
+  const CustomListCardWidget({
+    Key? key,
+    required this.movie,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      decoration: const BoxDecoration(
+        color: Colors.black54,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.network(API.REQUEST_IMG(movie.posterPath)),
+          Expanded(
+              child: Padding(
+            padding: const EdgeInsets.all(20),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                movie.title,
+                style: Theme.of(context).textTheme.headline6,
+                softWrap: true,
+                overflow: TextOverflow.visible,
+              ),
+              const Spacer(),
+              Text('Popularidade: ' + movie.popularity.toString()),
+              const SizedBox(
+                height: 10,
+              ),
+              Text('Popularidade: ' + movie.voteAverage.toString()),
+            ]),
+          ))
+        ],
+      ),
+    );
+  }
+}
+
+//Image.network(API.REQUEST_IMG(movie.posterPath));
